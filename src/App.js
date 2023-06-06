0575c68ccb1186ca8405c4a98d006f2a91f05f07
helga-umrikh <zetaumr@gmail.com>
2023-06-06 19:35:16 +0300

    add: App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./componentss/Header/Header";
+import Main from "./componentss/Main/Main";
+import Footer from "./componentss/Footer/Footer";
+import { Switch, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { dataMock } from "./dataMock";
+import FullView from "./componentss/FullView/FullView";
 
 function App() {
+  const localStorageData = localStorage.getItem("data");
+  const [data, setData] = useState(
+    localStorageData ? JSON.parse(localStorageData) : dataMock
+  );
+  const updateData = (newData) => {
+    localStorage.setItem("data", JSON.stringify(newData));
+    setData(newData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <Routes>
+        <Route
+          path="/"
+          element={<Main data={data} updateData={updateData} />}
+          exact
+        />
+
+        <Route
+          path="/tasks/:id"
+          element={<FullView data={data} />}
+        />
+      </Routes>
+
+      <Footer data={data}/>
     </div>
   );
 }
